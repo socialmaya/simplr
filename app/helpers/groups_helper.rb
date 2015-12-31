1486107c09773d4627c27432392c6ff7624a4c08
ethanwilkins <ethanforrestwilkins@gmail.com>
2015-12-30 20:12:16 -0500

    group_auth_helper

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,9 @@
 module GroupsHelper
+  def group_auth group
+    if current_user
+      return current_user.id.eql? group.user_id
+    else
+      return anon_token.eql? group.anon_token
+    end
+  end
 end
