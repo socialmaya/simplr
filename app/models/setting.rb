505b16a239a40fc724cd3162b3d3f7c3a204fa64
ethanwilkins <ethanforrestwilkins@gmail.com>
2016-02-07 03:56:20 -0500

    profile_card_bg_color

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -10,6 +10,6 @@ class Setting < ActiveRecord::Base
   
   def self.names
     { on: ['bg_fader_on', 'post_bg_fader_on', 'post_txt_fader_on'],
-      state: ['bg_color', 'post_bg_color', 'post_txt_color'] }
+      state: ['bg_color', 'post_bg_color', 'post_txt_color', 'profile_card_bg_color', 'profile_bg_color'] }
   end
 end
