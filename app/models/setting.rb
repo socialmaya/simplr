353664e09a07778114d790594f6338b42cff237f
ethanwilkins <ethanforrestwilkins@gmail.com>
2016-02-07 04:21:21 -0500

    profile_bg_fader

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -9,7 +9,7 @@ class Setting < ActiveRecord::Base
   end
   
   def self.names
-    { on: ['bg_fader_on', 'post_bg_fader_on', 'post_txt_fader_on'],
+    { on: ['bg_fader_on', 'post_bg_fader_on', 'post_txt_fader_on', 'profile_card_bg_fader_on', 'profile_bg_fader_on'],
       state: ['bg_color', 'post_bg_color', 'post_txt_color', 'profile_card_bg_color', 'profile_bg_color'] }
   end
 end
