1e1cf2d1e2bb4173e9d55ea28d32a12fa7495208
ethanwilkins <ethanforrestwilkins@gmail.com>
2016-02-28 19:44:32 -0500

    fibonacci

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,8 @@ module Simplr
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    # for loading custom libraries
+    config.autoload_paths += %W(#{config.root}/lib)
   end
 end
---
+++ lib/fibonacci.rb
@@ -0,0 +1,6 @@
+class Fibonacci
+  def self.fib n
+    return n if (0..1).include? n
+    fib(n - 1) + fib(n - 2)
+  end
+end
