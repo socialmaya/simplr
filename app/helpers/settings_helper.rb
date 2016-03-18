3d80aea18eadfa1cd28f5b6ea4e092c79abdb486
ethanwilkins <ethanforrestwilkins@gmail.com>
2016-03-17 22:12:52 -0400

    setting_titles

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -1,4 +1,11 @@
 module SettingsHelper
+  def setting_titles
+    { bg_fader_on: "BG Fader", post_bg_fader_on: "Post BG Fader", post_txt_fader_on: "Post Text Fader",
+      profile_card_bg_fader_on: "Profile Card BG Fader", profile_bg_fader_on: "Profile BG Fader",
+      bg_color: "BG", post_bg_color: "Post BG", post_txt_color: "Post Text",
+      profile_card_bg_color: "Profile Card BG", profile_bg_color: "Profile BG" }
+  end
+  
   def rgb_options setting, color
     # initializes options with nil color label
     options = [[color.to_s.upcase, nil]]
@@ -24,15 +31,5 @@ module SettingsHelper
       options[0] = ["#{color.to_s.upcase}:#{rgb_n}", rgb_n]
     end
     return options
-  end
-  
-  def color_options
-    [["Choose a color", nil],
-    ['red', 'rgb(255, 0, 0)'],
-    ['green', 'rgb(0, 255, 0)'],
-    ['blue', 'rgb(0, 0, 255)'],
-    ['purple', 'purple'],
-    ['yellow', 'yellow'],
-    ['pink', 'magenta']]
   end
 end
