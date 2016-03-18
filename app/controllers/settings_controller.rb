a9c33ee44d03cad8961efca1c004fd1e751171be
ethanwilkins <ethanforrestwilkins@gmail.com>
2016-03-17 21:24:04 -0400

    rgb_color_forms

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -22,10 +22,18 @@ class SettingsController < ApplicationController
   def update
     Setting.names.each do |category, names|
       for name in names
+        setting = current_user.settings.find_by_name(name)
         # makes sure not to reset color values unless user restores defaults and accounts for on/off setting params
-        if category.eql? :state and params[name.to_sym].present? or category.eql? :on or params[:restore_defaults]
-          current_user.settings.find_by_name(name).update category => \
-            (params[:restore_defaults] ? (category.eql?(:on) ? false : "") : params[name.to_sym])
+        if params[:restore_defaults]
+          setting.update category => (category.eql?(:on) ? false : "")
+        elsif category.eql? :state
+          state_params = params[name.to_sym]
+          if state_params.any? {|key,val| val.present?}
+            setting.update category => \
+              "rgb(#{state_params[:r].to_i}, #{state_params[:g].to_i}, #{state_params[:b].to_i})"
+          end
+        elsif category.eql? :on
+          setting.update category => params[name.to_sym]
         end
       end
     end
--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -1,4 +1,31 @@
 module SettingsHelper
+  def rgb_options setting, color
+    # initializes options with nil color label
+    options = [[color.to_s.upcase, nil]]
+    # inserts rgb color range
+    (0..255).each do |n|
+      options << n
+    end
+    # replaces initial element if already set
+    if settings[setting.to_sym].present?
+      # strips string to just comma separated numbers
+      rgb_str = settings[setting.to_sym][4..-2]
+      # converts string to array of integers
+      rgb_array = rgb_str.split(', ').map{|n| n.to_i}
+      # gets relevant number from rgb array
+      rgb_n = case color
+      when :r
+        rgb_array[0]
+      when :g
+        rgb_array[1]
+      when :b
+        rgb_array[2]
+      end
+      options[0] = ["#{color.to_s.upcase}:#{rgb_n}", rgb_n]
+    end
+    return options
+  end
+  
   def color_options
     [["Choose a color", nil],
     ['red', 'rgb(255, 0, 0)'],
