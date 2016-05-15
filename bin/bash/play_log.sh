bbb044df3fe49e782890f435fc2826ce21bccf44
ethanwilkins <ethanforrestwilkins@gmail.com>
2016-05-14 22:55:55 -0400

    logstalgia_update

--- bin/bash/play_log.sh
+++ bin/bash/play_log.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-echo "\nFetching latest access log...\n"
+echo -e "\nFetching latest access log...\n"
 
 ssh root@104.131.182.123 'cp /var/log/nginx/access.log ~/ && chmod 755 ~/access.log'
 
 echo "get access.log" | sftp root@104.131.182.123
 
-echo "\nCleaning up...\n"
+echo -e "\nCleaning up...\n"
 
 ssh root@104.131.182.123 'rm ~/access.log'
 
-echo "Playing log...\n"
+echo -e "Playing log...\n"
 
 logstalgia -1280x720 ~/access.log
