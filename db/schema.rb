e2ad1566a1f60ab222aaaadef8c6f488ae67443e
ethanwilkins <ethanforrestwilkins@gmail.com>
2016-06-06 12:47:52 -0400

    shifting_to_bot_model1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160604071820) do
+ActiveRecord::Schema.define(version: 20160606160401) do
 
   create_table "bot_tasks", force: :cascade do |t|
     t.integer  "user_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160604071820) do
     t.text     "body"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "image"
   end
 
   create_table "comments", force: :cascade do |t|
