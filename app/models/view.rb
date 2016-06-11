7d9a8d30f7c04ed6fae88d6df05044265373c209
ethanwilkins <ethanforrestwilkins@gmail.com>
2016-06-11 19:30:40 -0400

    get_locale

--- app/models/view.rb
+++ app/models/view.rb
@@ -3,4 +3,24 @@ class View < ActiveRecord::Base
   belongs_to :group
   belongs_to :post
   belongs_to :comment
+  
+  def get_location
+    ip = self.ip_address
+    address = nil; locale = nil
+    geoip = GeoIP.new('GeoLiteCity.dat').city(ip)
+    if defined? geoip and geoip
+      if geoip.latitude and geoip.longitude
+        geocoder = Geocoder.search("#{geoip.latitude}, #{geoip.longitude}").first
+        if geocoder and geocoder.formatted_address
+          address = geocoder.formatted_address
+        end
+      end
+    end
+    locale = if address.present?
+      { address: address, lat: geoip.latitude, lon: geoip.longitude }
+    else
+      {}
+    end
+    return locale
+  end
 end
