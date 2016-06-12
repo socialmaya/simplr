9bf9a8c6cfa60071a00388ce9e4c6f00aa938051
ethanwilkins <ethanforrestwilkins@gmail.com>
2016-06-12 00:15:31 -0400

    anrcho?5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -191,7 +191,7 @@ class ApplicationController < ActionController::Base
   
   private
     def anrcho_to_proposals
-      if request.host.eql? 'anrcho.com' and request.original_url.size <= "https://anrcho.com/".size
+      if request.host.eql? 'anrcho.com' and request.original_url.count("/").eql? 2
         redirect_to proposals_path
       end
     end
