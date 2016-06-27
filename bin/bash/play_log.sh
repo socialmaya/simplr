fe957324c39873cbe88e88e0b2c78e88b78040e5
ethanwilkins <ethanforrestwilkins@gmail.com>
2016-06-27 13:48:05 -0400

    play_log_update

--- bin/bash/play_log.sh
+++ bin/bash/play_log.sh
@@ -2,13 +2,13 @@
 
 echo -e "\nFetching latest access log...\n"
 
-ssh root@104.131.182.123 'cp /var/log/nginx/access.log ~/ && chmod 755 ~/access.log'
+ssh root@socialmaya.com 'cp /var/log/nginx/access.log ~/ && chmod 755 ~/access.log'
 
-echo "get access.log" | sftp root@104.131.182.123
+echo "get access.log" | sftp root@socialmaya.com
 
 echo -e "\nCleaning up...\n"
 
-ssh root@104.131.182.123 'rm ~/access.log'
+ssh root@socialmaya.com 'rm ~/access.log'
 
 echo -e "Playing log...\n"
 
