d1d3fe410d63d58b180f3a12c728d7eacb1d988c
ethanwilkins <ethanforrestwilkins@gmail.com>
2016-07-16 13:42:09 -0400

    portal_const_removed

--- app/models/portal.rb
+++ app/models/portal.rb
@@ -1,14 +1,11 @@
 class Portal < ActiveRecord::Base
-  EXPIRES_AT = 1.week.from_now.to_datetime
-  REMAINING_USES = 5
-  
   before_create :gen_unique_token, :initialize_portal
     
   private
     # defaults to 5 uses and expires a week from now
     def initialize_portal
-      self.expires_at ||= EXPIRES_AT
-      self.remaining_uses ||= REMAINING_USES
+      self.expires_at ||= 1.week.from_now.to_datetime
+      self.remaining_uses ||= 5
     end
 
     def gen_unique_token
