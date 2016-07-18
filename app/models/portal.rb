8ea4a9e8ba3bf31a8bb7303079ff7cfdabf7c7ae
ethanwilkins <ethanforrestwilkins@gmail.com>
2016-07-18 13:08:51 -0400

    portal_form_defaults

--- app/models/portal.rb
+++ app/models/portal.rb
@@ -1,11 +1,17 @@
 class Portal < ActiveRecord::Base
   before_create :gen_unique_token, :initialize_portal
+  
+  DEFAULT_REMAINING_USES = 5
+  
+  def self.expiration_date
+    1.week.from_now.to_datetime
+  end
     
   private
     # defaults to 5 uses and expires a week from now
     def initialize_portal
-      self.expires_at ||= 1.week.from_now.to_datetime
-      self.remaining_uses ||= 5
+      self.expires_at ||= Portal.expiration_date
+      self.remaining_uses ||= DEFAULT_REMAINING_USES
     end
 
     def gen_unique_token
