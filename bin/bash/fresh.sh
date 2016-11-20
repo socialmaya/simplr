bbd28026077f8f302a42a561a66c95e764cc1beb
ethanwilkins <ethanforrestwilkins@gmail.com>
2016-11-20 14:47:45 -0500

    eh_update1

--- bin/bash/fresh.sh
+++ bin/bash/fresh.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ssh root@socialmaya.com 'service unicorn stop && cd /home/rails/simplr && git pull && service unicorn start'
+ssh root@anrcho.com 'service unicorn stop && cd /home/rails/simplr && git pull && service unicorn start'
