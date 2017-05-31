a6777b35b950d82ad3d547f42c2c4faaf0c1a830
ethanwilkins <ethanforrestwilkins@gmail.com>
2017-05-31 03:09:48 -0400

    views_index

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -1,2 +1,13 @@
 class ViewsController < ApplicationController
+  before_action :dev_only, only: [:index]
+  
+  def index
+    @views = View.all.reverse
+  end
+  
+  private
+  
+  def dev_only
+    dev?
+  end
 end
