d4ada804e928ab8ef3b90822f50c63f9464192d4
ethanwilkins <ethanforrestwilkins@gmail.com>
2017-09-10 06:51:35 -0400

    kristin_omgerrrrddd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -150,7 +150,7 @@ class UsersController < ApplicationController
     
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      @user = User.find_by_id params[:id]
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
