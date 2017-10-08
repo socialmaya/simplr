eba094fdcaf77e7569bff6ffda51d4c9db74b524
ethanwilkins <ethanforrestwilkins@gmail.com>
2017-10-08 18:18:06 -0400

    portal_life

--- app/helpers/portals_helper.rb
+++ app/helpers/portals_helper.rb
@@ -1,2 +1,10 @@
 module PortalsHelper
+  def portal_life_durations
+    options = [["How long should it stay open for?", nil],
+      ["1 week (default)", 7],
+      ["1 month", 30],
+      ["2 months", 60],
+      ["3 months", 90]]
+    return options
+  end
 end
