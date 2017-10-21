3726f884401ac89c9b4a375108338a619a9c05e7
ethanwilkins <ethanforrestwilkins@gmail.com>
2017-10-21 15:21:26 -0400

    unique_like1

--- app/models/like.rb
+++ app/models/like.rb
@@ -24,23 +24,32 @@ class Like < ActiveRecord::Base
   private
   
   def unique_to_item?
+    like_type = if self.love
+      :loves
+    elsif self.whoa
+      :whoas
+    elsif self.zen
+      :zens
+    else
+      :_likes
+    end
     if self.post_id
       if self.user_id
-        if Post.find(self.post_id).views.where(user_id: self.user_id).present?
+        if Post.find(self.post_id).send(like_type).where(user_id: self.user_id).present?
           errors.add :like, "Not unique like by user"
         end
       elsif self.anon_token
-        if Post.find(self.post_id).views.where(anon_token: self.anon_token).present?
+        if Post.find(self.post_id).send(like_type).where(anon_token: self.anon_token).present?
           errors.add :like, "Not unique like by anon"
         end
       end
     elsif self.proposal_id
       if self.user_id
-        if Proposal.find(self.proposal_id).views.where(user_id: self.user_id).present?
+        if Proposal.find(self.proposal_id).send(like_type).where(user_id: self.user_id).present?
           errors.add :like, "Not unique like of motion by user"
         end
       elsif self.anon_token
-        if Proposal.find(self.proposal_id).views.where(anon_token: self.anon_token).present?
+        if Proposal.find(self.proposal_id).send(like_type).where(anon_token: self.anon_token).present?
           errors.add :like, "Not unique like of motion by anon"
         end
       end
