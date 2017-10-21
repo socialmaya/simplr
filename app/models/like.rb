94644fcbc624d2ed866170f90a25f64161e946b2
ethanwilkins <ethanforrestwilkins@gmail.com>
2017-10-21 15:05:12 -0400

    unique_like

--- app/models/like.rb
+++ app/models/like.rb
@@ -11,6 +11,8 @@ class Like < ActiveRecord::Base
   
   before_create :gen_unique_token
   
+  validate :unique_to_item?, on: :create
+  
   scope :loves, -> { where love: true }
   scope :whoas, -> { where whoa: true }
   scope :zens, -> { where zen: true }
@@ -20,6 +22,30 @@ class Like < ActiveRecord::Base
   end
 
   private
+  
+  def unique_to_item?
+    if self.post_id
+      if self.user_id
+        if Post.find(self.post_id).views.where(user_id: self.user_id).present?
+          errors.add :like, "Not unique like by user"
+        end
+      elsif self.anon_token
+        if Post.find(self.post_id).views.where(anon_token: self.anon_token).present?
+          errors.add :like, "Not unique like by anon"
+        end
+      end
+    elsif self.proposal_id
+      if self.user_id
+        if Proposal.find(self.proposal_id).views.where(user_id: self.user_id).present?
+          errors.add :like, "Not unique like of motion by user"
+        end
+      elsif self.anon_token
+        if Proposal.find(self.proposal_id).views.where(anon_token: self.anon_token).present?
+          errors.add :like, "Not unique like of motion by anon"
+        end
+      end
+    end
+  end
   
   def gen_unique_token
     self.unique_token = SecureRandom.urlsafe_base64
