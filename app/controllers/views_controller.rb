44e604479986f403d56a4238d41df5f174b24651
ethanwilkins <ethanforrestwilkins@gmail.com>
2017-11-11 14:54:26 -0500

    v

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -12,6 +12,8 @@ class ViewsController < ApplicationController
       @unique_views << view unless @unique_views.any? { |v| v.locale.eql? view.locale }
     end
     @views = @unique_views.sort_by { |v| v.created_at }.reverse
+    get_most_viewed_locale
+    
   end
   
   def index
@@ -19,6 +21,20 @@ class ViewsController < ApplicationController
   end
   
   private
+  
+  def get_most_viewed_locale
+    # this first should be last view created
+    @most_viewed = @views.first
+    a = @user.views.count { |i| i.locale.eql? @most_viewed.locale }
+    # loops through all views by user until most viewed from locale is found
+    for v in @views.reverse # reverse to lead to first/last created
+      b = @user.views.count { |i| i.locale.eql? v.locale }
+      if a < b
+        @most_viewed = v
+        a = b
+      end
+    end
+  end
   
   def god_only
     redirect_to '/404' unless god?
